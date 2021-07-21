7e9e2a286babf2007c29936d062cf972f134e591
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-07-22 03:35:51 +0800

    Update scrcpystart.bat

--- scrcpystart.bat
+++ scrcpystart.bat
@@ -4,15 +4,14 @@ scrcpy -b 2m
 scrcpy --max-fps 15
 scrcpy --crop 1224:1440:0:0
 scrcpy --lock-video-orientation 0
-
-:PROMPT
+:PROMPT1
 SET /P RECORD=Do you want to start Recording? (Y/N):
 IF /I "%RECORD%" NEQ "Y" GOTO END
 
 scrcpy --record file.mp4
 scrcpy -r file.mkv
 
-:END
+:END1
 
 :PROMPT2
 SET /P RECORD=Disable mirroring while recording? (Y/N):
