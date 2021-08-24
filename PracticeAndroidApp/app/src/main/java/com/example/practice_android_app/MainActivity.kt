57b4b6cad301ec915034a4c90e64bfa7c9cdce23
Coalemus <61069122+Coalemus@users.noreply.github.com>
2021-08-25 07:10:51 +0800

    Added Display Message Activity

--- PracticeAndroidApp/app/src/main/java/com/example/practice_android_app/MainActivity.kt
+++ PracticeAndroidApp/app/src/main/java/com/example/practice_android_app/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.practice_android_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
