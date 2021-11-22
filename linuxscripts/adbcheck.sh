26d25f5875e2c9e94638b483885cea0393949053
Joey Flores <61069122+Coalemus@users.noreply.github.com>
2021-11-22 11:28:47 +0800

    Fixed prompts

--- linuxscripts/adbcheck.sh
+++ linuxscripts/adbcheck.sh
@@ -3,48 +3,74 @@ echo Enable USB Debugging in Phone.
 echo Click Allow in Phone prompt stating Trust.
 adb start-server
 adb devices
+## Colors ##
+ESC=$(printf '\033') RESET="${ESC}[0m" BLACK="${ESC}[30m" RED="${ESC}[31m"
+GREEN="${ESC}[32m" YELLOW="${ESC}[33m" BLUE="${ESC}[34m" MAGENTA="${ESC}[35m"
+CYAN="${ESC}[36m" WHITE="${ESC}[37m" DEFAULT="${ESC}[39m"
 
-:PROMPT
-SET /P LISTPKGS=Do you want to list all the Device's packages? (Y/N):
-IF /I %LISTPKGS% NEQ "Y" GOTO END
+### Color Functions ##
 
-adb shell pm list packages -s
-
-:END
-
-adb shell pm uninstall -k --user 0 com.coloros.assistantscreen
-adb shell pm uninstall -k --user 0 com.coloros.floatassistant
-adb shell pm uninstall -k --user 0 com.coloros.gamespace
-adb shell pm uninstall -k --user 0 com.coloros.filemanager
-adb shell pm uninstall -k --user 0 coloros.gamespaceui
-adb shell pm uninstall -k --user 0 com.coloros.childrenspace
-adb shell pm uninstall -k --user 0 com.facebook.appmanager
-adb shell pm uninstall -k --user 0 com.facebook.services
-adb shell pm uninstall -k --user 0 com.facebook.system
-adb shell pm uninstall -k --user 0 com.google.android.apps.magazines
-adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
-adb shell pm uninstall -k --user 0 com.google.android.youtube
-adb shell pm uninstall -k --user 0 com.heytap.browser
-adb shell pm uninstall -k --user 0 com.heytap.cloud
-adb shell pm uninstall -k --user 0 com.heytap.datamigration
-adb shell pm uninstall -k --user 0 com.heytap.habit.analysis
-adb shell pm uninstall -k --user 0 com.heytap.usercenter.overlay
-adb shell pm uninstall -k --user 0 com.heytap.datamigration
-adb shell pm uninstall -k --user 0 com.heytap.habit.analysis
-adb shell pm uninstall -k --user 0 com.heytap.usercenter
-adb shell pm uninstall -k --user 0 com.oppo.market
-adb shell pm uninstall -k --user 0 com.oppo.lfeh
-adb shell pm uninstall -k --user 0 com.oppo.aod
-adb shell pm uninstall -k --user 0 com.nearme.browser
-adb shell pm uninstall -k --user 0 com.nearme.themestore
-adb shell pm uninstall -k --user 0 com.mediatek.omacp
-adb shell pm uninstall -k --user 0 com.opera.preinstall
-adb shell pm uninstall -k --user 0 com.tencent.soter.soterserver
-adb shell pm uninstall -k --user 0 org.kman.AquaMail
-adb shell pm uninstall -k --user 0 com.dropboxchmod
-echo Can't delete these, don't know why.
-adb shell pm uninstall -k --user 0 com.heytap.market
-adb shell pm uninstall -k --user 0 com.heytap.themestore
-adb shell pm uninstall -k --user 0 com.nearme.gamecenter
-echo format is: adb shell pm uninstall -k --user 0 
-adb kill-server
+greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
+blueprint() { printf "${BLUE}%s${RESET}\n" "$1"; }
+redprint() { printf "${RED}%s${RESET}\n" "$1"; }
+yellowprint() { printf "${YELLOW}%s${RESET}\n" "$1"; }
+magentaprint() { printf "${MAGENTA}%s${RESET}\n" "$1"; }
+cyanprint() { printf "${CYAN}%s${RESET}\n" "$1"; }
+adbprompt() {
+    echo -ne "
+$(magentaprint "Would you like to erase realme bloat??")
+$(greenprint '1)') Continue
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    1)
+        adb shell pm uninstall -k --user 0 com.coloros.assistantscreen
+        adb shell pm uninstall -k --user 0 com.coloros.floatassistant
+        adb shell pm uninstall -k --user 0 com.coloros.gamespace
+        adb shell pm uninstall -k --user 0 com.coloros.filemanager
+        adb shell pm uninstall -k --user 0 coloros.gamespaceui
+        adb shell pm uninstall -k --user 0 com.coloros.childrenspace
+        adb shell pm uninstall -k --user 0 com.facebook.appmanager
+        adb shell pm uninstall -k --user 0 com.facebook.services
+        adb shell pm uninstall -k --user 0 com.facebook.system
+        adb shell pm uninstall -k --user 0 com.google.android.apps.magazines
+        adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+        adb shell pm uninstall -k --user 0 com.google.android.youtube
+        adb shell pm uninstall -k --user 0 com.heytap.browser
+        adb shell pm uninstall -k --user 0 com.heytap.cloud
+        adb shell pm uninstall -k --user 0 com.heytap.datamigration
+        adb shell pm uninstall -k --user 0 com.heytap.habit.analysis
+        adb shell pm uninstall -k --user 0 com.heytap.usercenter.overlay
+        adb shell pm uninstall -k --user 0 com.heytap.datamigration
+        adb shell pm uninstall -k --user 0 com.heytap.habit.analysis
+        adb shell pm uninstall -k --user 0 com.heytap.usercenter
+        adb shell pm uninstall -k --user 0 com.oppo.market
+        adb shell pm uninstall -k --user 0 com.oppo.lfeh
+        adb shell pm uninstall -k --user 0 com.oppo.aod
+        adb shell pm uninstall -k --user 0 com.nearme.browser
+        adb shell pm uninstall -k --user 0 com.nearme.themestore
+        adb shell pm uninstall -k --user 0 com.mediatek.omacp
+        adb shell pm uninstall -k --user 0 com.opera.preinstall
+        adb shell pm uninstall -k --user 0 com.tencent.soter.soterserver
+        adb shell pm uninstall -k --user 0 org.kman.AquaMail
+        adb shell pm uninstall -k --user 0 com.dropboxchmod
+        echo Can't delete these, don't know why.
+        adb shell pm uninstall -k --user 0 com.heytap.market
+        adb shell pm uninstall -k --user 0 com.heytap.themestore
+        adb shell pm uninstall -k --user 0 com.nearme.gamecenter
+        echo format is: adb shell pm uninstall -k --user 0 
+        adb kill-server
+        exit
+        ;;
+    0)
+        echo "Bye bye."
+        exit 1
+        ;;
+    *)
+        echo "Wrong option."
+        exit 1
+        ;;
+    esac
+}
+adbprompt
--- linuxscripts/scrcpyrec.sh
+++ linuxscripts/scrcpyrec.sh
@@ -1,18 +1,43 @@
-echo off
-:PROMPT1
-SET /P RECORD=Do you want to start Recording? (Y/N):
-IF /I "%RECORD%" NEQ "Y" GOTO END1
+## Colors ##
+ESC=$(printf '\033') RESET="${ESC}[0m" BLACK="${ESC}[30m" RED="${ESC}[31m"
+GREEN="${ESC}[32m" YELLOW="${ESC}[33m" BLUE="${ESC}[34m" MAGENTA="${ESC}[35m"
+CYAN="${ESC}[36m" WHITE="${ESC}[37m" DEFAULT="${ESC}[39m"
 
-scrcpy --record file.mp4
-scrcpy -r file.mkv
+### Color Functions ##
 
-:END1
-
-:PROMPT2
-SET /P RECORD=Disable mirroring while recording? (Y/N):
-IF /I "%RECORD%" NEQ "Y" GOTO END2
-
-scrcpy --no-display --record file.mp4
-scrcpy -Nr file.mkv
-
-:END2
+greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
+blueprint() { printf "${BLUE}%s${RESET}\n" "$1"; }
+redprint() { printf "${RED}%s${RESET}\n" "$1"; }
+yellowprint() { printf "${YELLOW}%s${RESET}\n" "$1"; }
+magentaprint() { printf "${MAGENTA}%s${RESET}\n" "$1"; }
+cyanprint() { printf "${CYAN}%s${RESET}\n" "$1"; }
+adbprompt() {
+    echo -ne "
+$(magentaprint "What type of recording will you do?")
+$(blueprint '2)') Record and Save
+$(greenprint '1)') Record only
+$(redprint '0)') Exit
+Choose an option:  "
+    read -r ans
+    case $ans in
+    2)
+        scrcpy --record file.mp4
+        scrcpy -r file.mkv
+        exit
+        ;;
+    1)
+        scrcpy --no-display --record file.mp4
+        scrcpy -Nr file.mkv
+        exit
+        ;;
+    0)
+        echo "Bye bye."
+        exit 1
+        ;;
+    *)
+        echo "Wrong option."
+        exit 1
+        ;;
+    esac
+}
+adbprompt
